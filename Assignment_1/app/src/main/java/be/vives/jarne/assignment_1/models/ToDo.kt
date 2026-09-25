8f16d10cf53e3d1fb75293b2a51c553f20129521
JarneAdam <jarne.adam3@gmail.com>
2026-09-25 14:20:22 +0200

    Added calculated fields in ToDo class

--- Assignment_1/app/src/main/java/be/vives/jarne/assignment_1/models/ToDo.kt
+++ Assignment_1/app/src/main/java/be/vives/jarne/assignment_1/models/ToDo.kt
@@ -2,6 +2,10 @@ package be.vives.jarne.assignment_1.models
 
 import java.util.Date
 
+enum class Status {
+    NEW, ASSIGNED, FINISHED
+}
+
 data class ToDo(
     var number: Int,
     var title: String,
@@ -15,4 +19,33 @@ data class ToDo(
     var developmentDone: Boolean = false,
     var reviewAndTestingDone: Boolean = false,
     var acceptanceDone: Boolean = false
-)
+) {
+    val status: Status
+        get() {
+            return if (assignedToUser != null && finishedOnDate != null) {
+                Status.FINISHED
+            } else if (assignedToUser != null) {
+                Status.ASSIGNED
+            } else {
+                Status.NEW
+            }
+        }
+
+    val statusDescription: String
+        get() = when (status) {
+            Status.NEW -> "New"
+            Status.ASSIGNED -> "Assigned"
+            Status.FINISHED -> "Finished"
+        }
+
+    val timeRemaining: Int
+        get() {
+            return when {
+                acceptanceDone -> 0
+                reviewAndTestingDone -> (timeEstimated * 0.10).toInt()
+                developmentDone -> (timeEstimated * 0.30).toInt()
+                analysisDone -> (timeEstimated * 0.85).toInt()
+                else -> timeEstimated
+            }
+        }
+}
